3afe213bbb7298fd8fc28ab305c5766271225f4f
Matteo Sala <matteo111997@gmail.com>
2024-03-13 16:53:50 +0100

    config commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laravel comics</title>
+    <title>Laravel Comics</title>
     @vite('resources/js/app.js')
 </head>
 <body>
+
     
+
 </body>
 </html>
